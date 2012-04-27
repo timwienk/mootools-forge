a3b6ca1635cbf982a300c8b88d317086b508637f
fakedarren <darren.waddell@gmail.com>
2012-04-27 17:09:04 +0100

    Updating / Bootstrappin' HTML

--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -41,40 +41,37 @@
 <hr />
 
 <div class="row">
-	<h3 class="blue"><span>Recently active</span></h3>
-		
-	<div class="meta">
-		<?php echo link_to('View all', '@developers') ?>
+	<div class="span6">
+		<h3 class="blue"><span>Recently active</span></h3>
+		<div class="meta">
+			<?php echo link_to('View all', '@developers') ?>
+		</div>
+		<?php if ($authors->count()): ?>
+		<ul class="authors thumbnails">
+			<?php foreach ($authors as $author): ?>
+			<li class="span2">
+				<a href="<?php echo url_for('user', array('username' => $author->getUsername())) ?>">
+					<span class="avatar"><?php echo avatar_for($author) ?></span>
+					<span class="name"><?php echo $author->getFullName() ?></span>
+					<?php if ($author->getPluginsCount()): ?><span class="badge"><?php echo $author->getPluginsCount() ?></span><?php endif ?>
+				</a>
+			</li>	
+			<?php endforeach ?>
+		</ul>
+		<?php else: ?>
+		<p>No recently active users</p>
+		<?php endif ?>
 	</div>
-	
-	<?php if ($authors->count()): ?>
-	<ul class="authors">
-		<?php foreach ($authors as $author): ?>
-		<li>
-			<a href="<?php echo url_for('user', array('username' => $author->getUsername())) ?>">
-				<span class="avatar"><?php echo avatar_for($author) ?></span>
-				<span class="name"><?php echo $author->getFullName() ?></span>
-				<?php if ($author->getPluginsCount()): ?><span class="badge"><?php echo $author->getPluginsCount() ?></span><?php endif ?>				
-			</a>
-		</li>	
-		<?php endforeach ?>
-	</ul>
-	<?php else: ?>
-	<p>No recently active users</p>
-	<?php endif ?>
-	
-</div>
-
-<div class="row">
-	<h3 class="blue"><span>Popular tags</span></h3>
-	
-	<?php if ($terms->count()): ?>
-	<ul class="tags-list">
-		<?php foreach ($terms as $term): ?>
-		<li><?php echo link_to($term, 'browse', array('tag' => $term->getSlug())) ?></li>
-		<?php endforeach ?>
-	</ul>
-	<?php else: ?>
-	<p>No tags to show</p>
-	<?php endif; ?>
+	<div class="span3">
+		<h3 class="blue"><span>Popular tags</span></h3>
+		<?php if ($terms->count()): ?>
+		<ul class="tags-list">
+			<?php foreach ($terms as $term): ?>
+			<li><?php echo link_to($term, 'browse', array('tag' => $term->getSlug())) ?></li>
+			<?php endforeach ?>
+		</ul>
+		<?php else: ?>
+		<p>No tags to show</p>
+		<?php endif; ?>
+	</div>
 </div>
--- apps/frontend/modules/plugin/templates/viewSuccess.php
+++ apps/frontend/modules/plugin/templates/viewSuccess.php
@@ -2,164 +2,152 @@
 <?php use_helper('XssSafe') ?>
 
 <?php $raw = $sf_data->getRaw('plugin'); ?>
-<div class="block" id="project">
-	<h2 class="green"><span><?php echo $plugin->getTitle() ?>
+<div class="page-header">
+	<h1><?php echo $plugin->getTitle() ?>
 	<?php if ($plugin->getStableTag()): ?><em class="version"><?php echo $plugin->getStableTag()->getName() ?></em><?php endif ?>
-	</span></h2>
-
-	<div class="block" id="project-desc">
-		<?php if ($plugin->getScreenshot()): ?><p id="thumb"><a href="<?php echo url_for_screenshot($plugin->getScreenshot()) ?>" class="remooz"><span class="project_thumb"><?php echo thumbnail_for($plugin) ?></span></a></p><?php endif; ?>
+	</h1>
+</div>
+<div class="row">
+	<div>
+		<?php if ($plugin->getScreenshot()): ?>
+		<p><a href="<?php echo url_for_screenshot($plugin->getScreenshot()) ?>"><?php echo thumbnail_for($plugin) ?></a></p>
+		<?php endif; ?>
 		<?php echo esc_xsssafe($raw->getDescription()); ?>
 	</div>
-
-	<hr />
-
-	<div id="plugin-links" class="link-boxes block">
+	<div class="row">
 		<?php if ($plugin->getDocsUrl()): ?>
 		<a href="<?php echo $plugin->getDocsUrl() ?>">Docs</a>
 		<?php endif; ?>
 		<?php if ($plugin->getDemourl()): ?>
 		<a href="<?php echo $plugin->getDemourl() ?>">Demo</a>
 		<?php endif; ?>
-
-    <?php if ($plugin->getStableTag()): ?>
-		<?php echo link_to('Download', 'download', array('project' => $plugin->getSlug(), 'tag' => $plugin->getStableTag()->getName())) ?>
-    <?php endif ?>
-
+	    <?php if ($plugin->getStableTag()): ?>
+			<?php echo link_to('Download', 'download', array('project' => $plugin->getSlug(), 'tag' => $plugin->getStableTag()->getName())) ?>
+	    <?php endif ?>
 		<?php if ($sf_user->isAuthenticated() && $sf_user->ownsPlugin($plugin)): ?>
 		<?php echo link_to('Update', '@pluginupdate?slug=' . $plugin->getSlug(), array('id' => 'plugin-update')) ?>
 		<?php endif ?>
 	</div>
 
 	<?php if ($sf_user->isAuthenticated() && $sf_user->ownsPlugin($plugin)): ?>
-	<form action="<?php echo url_for('plugin/add') ?>" method="post" accept-charset="utf-8" id="update-form" class="block">
+	<form action="<?php echo url_for('plugin/add') ?>" method="post" accept-charset="utf-8">
 		<input type="hidden" name="id" value="<?php echo $plugin->getSlug() ?>" />
 	</form>
 	<?php endif ?>
 
-	<hr />
+	<div class="row">
+		<div class="span5">
+			<h3>Details</h3>
+			<dl>
+				<dt>Author</dt>
+				<dd><?php echo link_to($plugin->getAuthor()->getFullname(), 'user', array('username' => $plugin->getAuthor()->getUsername())) ?></dd>
 
-	<div class="block span-8 colborder">
-		<h3 class="blue"><span>Details</span></h3>
-
-		<dl class="table">
-			<dt>Author</dt>
-			<dd><?php echo link_to($plugin->getAuthor()->getFullname(), 'user', array('username' => $plugin->getAuthor()->getUsername())) ?></dd>
-
-			<?php if ($plugin->getStableTag()): ?>
-			<dt>Current version</dt>
-			<dd><?php echo $plugin->getStableTag()->getName() ?></dd>
-			<?php endif ?>
-
-			<dt>GitHub</dt>
-			<?php $github = sprintf('%s/%s', $plugin->getGithubuser(), $plugin->getGithubrepo()); ?>
-			<dd><a href="http://github.com/<?php echo $github; ?>/"><?php echo $github ?></a></dd>
-
-			<dt>Downloads</dt>
-			<dd><?php echo $plugin->getDownloadsCount() ?></dd>
-
-			<?php if ($plugin->getCategory()): ?>
-			<dt>Category</dt>
-			<dd><?php echo link_to($plugin->getCategory()->getTitle(), 'browse', array('category' => $plugin->getCategory()->getSlug())) ?></dd>
-			<?php endif ?>
-
-			<?php if ($termsTags->count() > 0): ?>
-			<dt>Tags</dt>
-			<dd id="plugin-tags">
-				<ul class="tags-list">
-					<?php foreach ($termsTags as $term): ?>
-					<?php $term = $term->getTerm(); ?>
-					<li><?php echo link_to($term, 'browse', array('tag' => $term->getSlug())) ?></li>
-					<?php endforeach ?>
-				</ul>
-			</dd>
-			<?php endif ?>
-
-			<dt>Report</dt>
-			<dd><a href="http://github.com/<?php echo $github; ?>/issues">GitHub Issues</a></dd>
-
-			<?php if ($sf_user->hasCredential('admin')): ?>
-			<dt>Admin</dt>
-			<dd><?php echo link_to('Delete', 'plugindelete', array('slug' => $plugin->getSlug()), array('id' => 'plugin-delete')) ?></dd>
-			<?php endif ?>
-		</dl>
-	</div>
-
-	<div class="block span-8 last">
-		<h3 class="blue"><span>Releases</span></h3>
-		<ul class="versions-list">
-			<?php foreach($tags as $tag): ?>
-				<li><?php echo link_to($tag->getName(), 'download', array('project' => $plugin->getSlug(), 'tag' => $tag->getName())) ?></li>
-			<?php endforeach ?>
-		</ul>
-
-		<?php if ($dependencies->count()): ?>
-		<hr />
-
-		<h3 class="blue"><span>Dependencies</span></h3>
-
-		<ul>
-			<?php $deps = array(); ?>
-			<?php foreach ($dependencies as $dep): ?>
-				<?php if ($dep->getPluginTag()):
-					$plugin = $dep->getPluginTag()->getPlugin();
-				?>
-				<li><?php echo link_to($plugin->getSlug() . '/' . $dep->getVersion(), 'plugin', array('slug' => $plugin->getSlug())) ?></li>
-				<?php else:
-					if (!isset($deps[$dep->getScope() . '/' . $dep->getVersion()]))
-						$deps[$dep->getScope() . '/' . $dep->getVersion()] = array();
-					$deps[$dep->getScope() . '/' . $dep->getVersion()][] = $dep->getComponent();
-				?>
+				<?php if ($plugin->getStableTag()): ?>
+				<dt>Current version</dt>
+				<dd><?php echo $plugin->getStableTag()->getName() ?></dd>
 				<?php endif ?>
-			<?php endforeach ?>
 
-			<?php foreach ($deps as $scope => $components):
-				$components = array_unique($components);
-			?>
-			<li>
-				<?php echo $scope ?>:
+				<dt>GitHub</dt>
+				<?php $github = sprintf('%s/%s', $plugin->getGithubuser(), $plugin->getGithubrepo()); ?>
+				<dd><a href="http://github.com/<?php echo $github; ?>/"><?php echo $github ?></a></dd>
 
-				<?php if (sizeof($components) == 1): ?>
-				<?php echo $components[0] ?>
-				<?php else: ?>
+				<dt>Downloads</dt>
+				<dd><?php echo $plugin->getDownloadsCount() ?></dd>
+
+				<?php if ($plugin->getCategory()): ?>
+				<dt>Category</dt>
+				<dd><?php echo link_to($plugin->getCategory()->getTitle(), 'browse', array('category' => $plugin->getCategory()->getSlug())) ?></dd>
+				<?php endif ?>
+
+				<?php if ($termsTags->count() > 0): ?>
+				<dt>Tags</dt>
+				<dd>
+					<ul>
+						<?php foreach ($termsTags as $term): ?>
+						<?php $term = $term->getTerm(); ?>
+						<li><?php echo link_to($term, 'browse', array('tag' => $term->getSlug())) ?></li>
+						<?php endforeach ?>
+					</ul>
+				</dd>
+				<?php endif ?>
+
+				<dt>Report</dt>
+				<dd><a href="http://github.com/<?php echo $github; ?>/issues">GitHub Issues</a></dd>
+
+				<?php if ($sf_user->hasCredential('admin')): ?>
+				<dt>Admin</dt>
+				<dd><?php echo link_to('Delete', 'plugindelete', array('slug' => $plugin->getSlug()), array('id' => 'plugin-delete')) ?></dd>
+				<?php endif ?>
+			</dl>
+		</div>
+
+		<div class="span4">
+			<div class="row">
+				<h3>Releases</h3>
 				<ul>
-					<?php foreach ($components as $component): ?>
-					<li><?php echo $component ?></li>
+					<?php foreach($tags as $tag): ?>
+						<li><?php echo link_to($tag->getName(), 'download', array('project' => $plugin->getSlug(), 'tag' => $tag->getName())) ?></li>
 					<?php endforeach ?>
 				</ul>
-				<?php endif ?>
-			</li>
-			<?php endforeach ?>
-		</ul>
-		<?php endif ?>
+			</div>
+			<?php if ($dependencies->count()): ?>
+			<div class="row">
+				<h3>Dependencies</h3>
+				<ul>
+					<?php $deps = array(); ?>
+					<?php foreach ($dependencies as $dep): ?>
+						<?php if ($dep->getPluginTag()):
+							$plugin = $dep->getPluginTag()->getPlugin();
+						?>
+						<li><?php echo link_to($plugin->getSlug() . '/' . $dep->getVersion(), 'plugin', array('slug' => $plugin->getSlug())) ?></li>
+						<?php else:
+							if (!isset($deps[$dep->getScope() . '/' . $dep->getVersion()]))
+								$deps[$dep->getScope() . '/' . $dep->getVersion()] = array();
+							$deps[$dep->getScope() . '/' . $dep->getVersion()][] = $dep->getComponent();
+						?>
+						<?php endif ?>
+					<?php endforeach ?>
+					<?php foreach ($deps as $scope => $components):
+						$components = array_unique($components);
+					?>
+					<li>
+						<?php echo $scope ?>:
 
+						<?php if (sizeof($components) == 1): ?>
+						<?php echo $components[0] ?>
+						<?php else: ?>
+						<ul>
+							<?php foreach ($components as $component): ?>
+							<li><?php echo $component ?></li>
+							<?php endforeach ?>
+						</ul>
+						<?php endif ?>
+					</li>
+					<?php endforeach ?>
+				</ul>
+			</div>
+			<?php endif ?>
+		</div>
 	</div>
 
-	<hr class="clear" />
-
-	<div class="block section">
-		<h3 class="blue"><span>How to use</span></h3>
-
+	<div class="row">
+		<h3>How to use</h3>
 		<?php echo esc_xsssafe($raw->getHowtouse()); ?>
 	</div>
 
 	<?php if ($sections->count()): ?>
 	<?php foreach ($sections as $section): ?>
-	<hr />
-	<div class="block section">
-		<h3 class="blue"><span><?php echo $section->getTitle(); ?></span></h3>
+	<div class="row">
+		<h3><?php echo $section->getTitle(); ?></h3>
 		<?php echo esc_xsssafe($section->getRawValue()->getContent()); ?>
 	</div>
 	<?php endforeach ?>
 	<?php endif ?>
 
 	<?php if ($screenshots->count()): ?>
-	<hr />
-
-	<div class="block section">
-		<h3 class="blue"><span>Screenshots</span></h3>
-
-		<ul class="screenshots-list">
+	<div class="row">
+		<h3>Screenshots</h3>
+		<ul>
 			<?php foreach ($screenshots as $screenshot): ?>
 			<li><a href="<?php echo url_for_screenshot($screenshot) ?>" title="<?php echo $screenshot->getTitle() ?>" class="remooz"><?php echo thumbnail_for($screenshot) ?></a></li>
 			<?php endforeach ?>
@@ -167,11 +155,9 @@
 	</div>
 	<?php endif ?>
 
-	<hr class="clear" />
-
-	<div class="block section">
-		<h3 class="blue"><span>Discuss</span></h3>
-		<p class="about" style="color: #700"><strong>A note on comments here</strong>: These comments are moderated. No comments will show up until they are approved. Comments that are not productive (i.e. inflammatory, rude, etc) will not be approved.
+	<div class="row">
+		<h3>Discuss</h3>
+		<p style="color: #700"><strong>A note on comments here</strong>: These comments are moderated. No comments will show up until they are approved. Comments that are not productive (i.e. inflammatory, rude, etc) will not be approved.
 		</p>
 		<p class="about" style="color: #700">Found a bug in this plugin? Please report it <a href="http://github.com/<?php echo $github; ?>/issues">this repository's Github Issues</a>.</p>
 		<style>
